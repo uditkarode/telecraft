44a668c48b86723e3239497ea70368be4fa80d90
Udit Karode <udit.karode@gmail.com>
2021-01-17 00:29:41 +0530

    [feat:youtube] combat ratelimit with key list

--- packages/youtube/src/index.ts
+++ packages/youtube/src/index.ts
@@ -12,7 +12,7 @@ const createError = (...str: string[]) =>
 const YoutubeLive: Plugin<{
 	enable: boolean;
 	videoId: string;
-	apiKey: string;
+	apiKey: [string];
 	fetchInterval?: number;
 	maxResults?: number;
 }> = opts => {
@@ -43,46 +43,71 @@ const YoutubeLive: Plugin<{
 					let stopFetching = false;
 					let lastCheckedTime: number;
 					let playersOnline = 0;
+					let keyIndex = 0;
 
 					const fetchMessages = async (): Promise<void> => {
 						if (stopFetching) return;
 
 						if (playersOnline > 0) {
-							const messageHolder = await axios.get(
-								"https://www.googleapis.com/youtube/v3/liveChat/messages" +
-									`?&maxResults=${opts.maxResults || 100}` +
-									"&part=id,snippet,authorDetails" +
-									`&liveChatId=${liveChatId}` +
-									`&key=${opts.apiKey}`,
-							);
+							axios
+								.get(
+									"https://www.googleapis.com/youtube/v3/liveChat/messages" +
+										`?&maxResults=${opts.maxResults || 100}` +
+										"&part=id,snippet,authorDetails" +
+										`&liveChatId=${liveChatId}` +
+										`&key=${opts.apiKey}`,
+								)
+								.then(messageHolder => {
+									const messages: Array<any> = messageHolder.data.items;
+									for (let message of messages) {
+										try {
+											if (message.snippet.hasDisplayContent) {
+												const sender = message.authorDetails.displayName;
+												const text = message.snippet.displayMessage;
+												const time = new Date(
+													message.snippet.publishedAt,
+												).getTime();
 
-							console.log("Got messages", messageHolder.data.items.length);
+												if (!lastCheckedTime || lastCheckedTime < time) {
+													lastCheckedTime = time;
+													const ytContext: MsgContext = {
+														text: text,
+														from: sender,
+													};
 
-							const messages: Array<any> = messageHolder.data.items;
-							for (let message of messages) {
-								try {
-									if (message.snippet.hasDisplayContent) {
-										const sender = message.authorDetails.displayName;
-										const text = message.snippet.displayMessage;
-										const time = new Date(
-											message.snippet.publishedAt,
-										).getTime();
-
-										if (!lastCheckedTime || lastCheckedTime < time) {
-											lastCheckedTime = time;
-											const ytContext: MsgContext = {
-												text: text,
-												from: sender,
-											};
-
-											const chatMessage = MCChat.message(ytContext);
-											server.send("tellraw @a " + JSON.stringify(chatMessage));
+													const chatMessage = MCChat.message(ytContext);
+													server.send(
+														"tellraw @a " + JSON.stringify(chatMessage),
+													);
+												}
+											}
+										} catch (e) {
+											console.log("Failed to get a message");
 										}
 									}
-								} catch (e) {
-									console.log("Failed to get a message");
-								}
-							}
+								})
+								.catch(error => {
+									if (error.response) {
+										if (error.response.status === 403) {
+											if (keyIndex === opts.apiKey.length)
+												throw createError(
+													"API key limit exhausted on all indexes!",
+												);
+											else {
+												console.log(
+													`API limit reached on key ${
+														keyIndex + 1
+													}, continuing with the next key.`,
+												);
+												keyIndex += 1;
+											}
+										}
+									} else {
+										throw createError(
+											"Could not make a request to YouTube API, check your connection!",
+										);
+									}
+								});
 						}
 
 						await sleep(opts.fetchInterval || 2000);
